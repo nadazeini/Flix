fe2f37befba9421579e3c41199518113597407cd
Nada Zeini <nada.elzeini@sjsu.edu>
2020-02-13 22:34:27 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Nada Zeini on 2/13/20.
+//  Copyright © 2020 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
