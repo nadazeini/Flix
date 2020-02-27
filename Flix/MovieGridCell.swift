7db0c3964f748a9cda67051163f0da94679f4139
Nada Zeini <nada.elzeini@sjsu.edu>
2020-02-27 15:56:55 -0800

    done with collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nada Zeini on 2/27/20.
+//  Copyright © 2020 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
