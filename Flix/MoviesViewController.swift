d0da4af3bc34bc0ce0ea1db11b5de9af582c30f6
Nada Zeini <nada.elzeini@sjsu.edu>
2020-02-27 02:16:39 -0800

    movie details view done

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -11,6 +11,7 @@ import AlamofireImage
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -51,6 +52,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("loading up details");
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
